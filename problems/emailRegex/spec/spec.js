f2b9ca14e1ce3ed6011e2966ec88f628275aed9c
Hack Reactor Students <accounts@hackreactor.com>
2013-03-25 09:31:58 -0700

    Email Regexp passing some cases

--- problems/emailRegex/spec/spec.js
+++ problems/emailRegex/spec/spec.js
@@ -27,26 +27,3 @@ describe('emailValidator', function() {
   });
 
 });
-
-
-niceandsimple@example.com
-very.common@example.com
-a.little.lengthy.but.fine@dept.example.com
-disposable.style.email.with+symbol@example.com
-user@[IPv6:2001:db8:1ff::a0b:dbd0]
-"much.more unusual"@example.com
-"very.unusual.@.unusual.com"@example.com
-"very.(),:;<>[]\".VERY.\"very@\\ \"very\".unusual"@strange.example.com
-postbox@com
-admin@mailserver1
-!#$%&'*+-/=?^_`{}|~@example.org
-"()<>[]:,;@\\\"!#$%&'*+-/=?^_`{}| ~.a"@example.org
-" "@example.org
-
-
-Abc.example.com
-A@b@c@example.com
-a"b(c)d,e:f;g<h>i[j\k]l@example.com
-just"not"right@example.com
-this is"not\allowed@example.com
-this\ still\"not\\allowed@example.com
